adf9f1304af168deecde2c4ad130ae9eb259d94d
Julien TESSIEAU <julien.tessieau@gmail.com>
2021-07-05 13:59:53 +0100

    feat: add a 405 error page
    - I change the header to http response code 405 and include a 405
      error page when the method is not allowed according to the
      router settings.

--- public/index.php
+++ public/index.php
@@ -26,11 +26,13 @@ switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         // ... 404 Not Found
         http_response_code(404);
-        include '../src/errors/404.html';
+        include '../template/errors/404.html';
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
         // ... 405 Method Not Allowed
+        http_response_code(405);
+        include '../template/errors/405.html';
         break;
     case FastRoute\Dispatcher::FOUND:
         $handler = $routeInfo[1];
